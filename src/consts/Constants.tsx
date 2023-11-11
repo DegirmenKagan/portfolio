a879e38ebaa4991ae2ee4c8b9c20ca7de13aab4d
Kağan Değirmen <kagandegirmen@gmail.com>
2023-11-11 13:59:14 +0300

    Constants file initialized

---
+++ src/consts/Constants.tsx
@@ -0,0 +1,6 @@
+export var Constants = {
+  github: "https://github.com/DegirmenKagan",
+  youtube: "https://www.youtube.com/@Filmiyorum",
+  linkedin: "https://www.linkedin.com/in/degirmenkagan/",
+  cv: "../assets/KAGAN_DEGIRMEN_CV.pdf",
+};
